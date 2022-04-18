7b81a03189d0493dfc6de3a1687b4c6ee915e2cd
Omar-macbook <omar987@hotmail.co.uk>
2022-04-18 19:39:57 +0100

    final commit

--- src/Trip.js
+++ src/Trip.js
@@ -11,25 +11,27 @@ export default function Trip(props) {
 		title,
 	} = props;
 	return (
-		<div className="trip--div">
-			<div className="trip--image">
-				<img src={imageUrl} alt="location" />
-			</div>
-			<div className="trip--info">
-				<div className="location-top">
-					<LocationOnIcon />
-					<h4>{location}</h4>
-					<a href={googleMapsUrl} target="_blank" rel="noreferrer">
-						View on Google Maps
-					</a>
+		<>
+			<div className="trip--div">
+				<div className="trip--image">
+					<img src={imageUrl} alt="location" />
 				</div>
-				<h2 className="location-title">{title}</h2>
-				<p className="location-dates">
-					{startDate} - {endDate}
-				</p>
-				<p className="location-desc">{description}</p>
+				<div className="trip--info">
+					<div className="location-top">
+						<LocationOnIcon />
+						<h4>{location}</h4>
+						<a href={googleMapsUrl} target="_blank" rel="noreferrer">
+							View on Google Maps
+						</a>
+					</div>
+					<h2 className="location-title">{title}</h2>
+					<p className="location-dates">
+						{startDate} - {endDate}
+					</p>
+					<p className="location-desc">{description}</p>
+				</div>
+				<hr />
 			</div>
-			<hr />
-		</div>
+		</>
 	);
 }
